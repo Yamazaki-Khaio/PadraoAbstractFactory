5d289d2bc9ec18759fb85931d4e7d1556a06cbd9
Yamazaki Khaio <91100781+Yamazaki-Khaio@users.noreply.github.com>
2022-07-05 13:01:54 -0300

    Update CorFactory.java

--- src/AbstractFactoryComUso/CorFactory.java
+++ src/AbstractFactoryComUso/CorFactory.java
@@ -11,9 +11,7 @@ public class CorFactory implements AbstractFactory<Cor>{
 
     @Override
     public Cor criar(String tipo) {
-        if("Marrom".equalsIgnoreCase(tipo)) {
-            return new Marrom();
-        } else if("Preto".equalsIgnoreCase(tipo)) {
+        if("Preto".equalsIgnoreCase(tipo)) {
             return new Preto();
         } else if("Branco".equalsIgnoreCase(tipo)) {
             return new Branco();
