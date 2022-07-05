3330088f85cb59e510df8285ad28878813b6e004
Yamazaki Khaio <91100781+Yamazaki-Khaio@users.noreply.github.com>
2022-07-05 12:55:49 -0300

    Update FactoryProvider.java

--- src/AbstractFactoryComUso/FactoryProvider.java
+++ src/AbstractFactoryComUso/FactoryProvider.java
@@ -1,15 +1,17 @@
 package AbstractFactoryComUso;
 
 import AbstractFactorySemUso.Animal;
-
+import AbstractFactorySemUso.Cor;
 public class FactoryProvider {
 
-    public static AbstractFactory<Animal> getFactory(String escolha) {
+       public static AbstractFactory getFactory(String escolha) {
         if("Animal".equalsIgnoreCase(escolha)) {
             return new AnimalFactory();
+        } else if("Cor".equalsIgnoreCase(escolha)) {
+            return new CorFactory();
+        }
 
-    }
-		return null;
+        return null;
     }
 }
     
