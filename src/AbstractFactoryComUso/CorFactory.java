771a1ec21cc683d05b0f807ceb7616805ccf8b62
Yamazaki Khaio <91100781+Yamazaki-Khaio@users.noreply.github.com>
2022-07-05 12:53:05 -0300

    Create CorFactory.java

---
+++ src/AbstractFactoryComUso/CorFactory.java
@@ -0,0 +1,14 @@
+public class CorFactory implements AbstractFactory<Cor>{
+
+    @Override
+    public Cor criar(String tipo) {
+        if("Marrom".equalsIgnoreCase(tipo)) {
+            return new Marrom();
+        } else if("Preto".equalsIgnoreCase(tipo)) {
+            return new Preto();
+        } else if("Branco".equalsIgnoreCase(tipo)) {
+            return new Branco();
+        }
+        return null;
+    }
+}
